fcb409908cda6328d95a359a1f72e5e34f20ac2a
Mehmet Selcuk Öner <mehmetso@uia.no>
2024-01-18 00:33:48 +0100

    test-push

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ikt205 losturcos are going to make an work schedule/tracker app. our experienced and talented developer team are ready and eager for this challenge</Text>
       <StatusBar style="auto" />
     </View>
   );
