01667d458adaa2a9971a899564f220b602a2f047
Mehmet Akif Gültekin <mehmetag@uia.no>
2024-02-01 23:36:29 +0100

    added firestore, expo router and empty CRUD functions

---
+++ CRUD/delete.jsx
@@ -0,0 +1,9 @@
+
+
+
+export const Delete = () => {
+    // code for deleting
+    // doc: https://firebase.google.com/docs/firestore/manage-data/delete-data?hl=en&authuser=0
+
+
+}
---
+++ CRUD/update.jsx
@@ -0,0 +1,6 @@
+
+
+export const Update = () => {
+    // code for Updating
+    // doc: https://firebase.google.com/docs/firestore/manage-data/add-data#update-data
+}
